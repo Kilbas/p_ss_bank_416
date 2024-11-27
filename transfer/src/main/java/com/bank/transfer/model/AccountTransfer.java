15f2f464eedf1e58351e55d0f995d9a3552887a8
Anton200021243 <mortal0011@gmail.com>
2024-11-27 22:41:57 +0300

    добавлены аннотации для валидации сущностей

--- transfer/src/main/java/com/bank/transfer/model/AccountTransfer.java
+++ transfer/src/main/java/com/bank/transfer/model/AccountTransfer.java
@@ -8,6 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 @Entity
@@ -17,18 +20,22 @@ public class AccountTransfer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private long id;
 
-    @Column(name = "account_number")
+    @Column(name = "account_number", nullable = false, unique = true)
+    @Positive(message = "Номер счёта должен быть положительным")
     private long number;
 
-    @Column(name = "amount")
+    @Column(name = "amount", nullable = false)
+    @DecimalMin(value = "0.01", message = "Сумма перевода должна быть больше 0.01")
+    @Digits(integer = 20, fraction = 2, message = "Сумма перевода должна содержать не более 20 цифр")
     private BigDecimal amount;
 
     @Column(name = "purpose")
     private String purpose;
 
-    @Column(name = "account_details_id")
+    @Column(name = "account_details_id", nullable = false)
+    @Positive(message = "ID должен быть положительным")
     private long accountDetailsId;
 }
