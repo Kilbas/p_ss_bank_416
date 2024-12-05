241f9ec32475bee5ec0c7c737579a705bda3eb0f
Anton200021243 <mortal0011@gmail.com>
2024-12-05 11:52:05 +0300

    добавлены аннотации swagger для AuditController

--- transfer/src/main/java/com/bank/transfer/controller/AuditController.java
+++ transfer/src/main/java/com/bank/transfer/controller/AuditController.java
@@ -4,6 +4,9 @@ import com.bank.transfer.dto.AuditDTO;
 import com.bank.transfer.mapper.AuditMapper;
 import com.bank.transfer.model.Audit;
 import com.bank.transfer.service.AuditService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,12 @@ public class AuditController {
     private final AuditService auditService;
     private final AuditMapper auditMapper;
 
+    @Operation(summary = "Получение всех записей аудита",
+            description = "Возвращает список всех записей аудита в формате AuditDTO.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список успешно получен"),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера")
+    })
     @GetMapping
     public ResponseEntity<List<AuditDTO>> getAllAudits() {
         List<Audit> audits = auditService.getAllAudit();
@@ -30,6 +39,13 @@ public class AuditController {
         return ResponseEntity.ok(auditDTOs);
     }
 
+    @Operation(summary = "Получение записи аудита по ID",
+            description = "Возвращает запись аудита по переданному идентификатору.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Запись успешно получена"),
+            @ApiResponse(responseCode = "404", description = "Запись с указанным ID не найдена"),
+            @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<AuditDTO> getAuditById(@PathVariable long id) {
         Audit audit = auditService.getAuditById(id);
