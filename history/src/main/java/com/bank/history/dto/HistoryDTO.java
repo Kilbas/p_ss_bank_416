bc8d436803189a38286b1692818a140853786b1e
Ambjorn97 <turpuhanov.amir@yandex.ru>
2024-12-09 12:36:57 +0300

    исправил тип данных c integer на lond в entity и dto, заменил hard-coded сеттеры и геттеры на аннотации lombok

--- history/src/main/java/com/bank/history/dto/HistoryDTO.java
+++ history/src/main/java/com/bank/history/dto/HistoryDTO.java
@@ -1,88 +1,37 @@
 package com.bank.history.dto;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Objects;
 
+@Getter
+@Setter
 @NoArgsConstructor
 
 public class HistoryDTO {
 
-    private Integer id;
+    private Long id;
 
-    private Integer transferAuditId;
+    private Long transferAuditId;
 
-    private Integer profileAuditId;
+    private Long profileAuditId;
 
-    private Integer accountAuditId;
+    private Long accountAuditId;
 
-    private Integer antiFraudAuditId;
+    private Long antiFraudAuditId;
 
-    private Integer publicBankInfoAuditId;
+    private Long publicBankInfoAuditId;
 
-    private Integer authorizationAuditId;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getTransferAuditId() {
-        return transferAuditId;
-    }
-
-    public void setTransferAuditId(Integer transferAuditId) {
-        this.transferAuditId = transferAuditId;
-    }
-
-    public Integer getProfileAuditId() {
-        return profileAuditId;
-    }
-
-    public void setProfileAuditId(Integer profileAuditId) {
-        this.profileAuditId = profileAuditId;
-    }
-
-    public Integer getAccountAuditId() {
-        return accountAuditId;
-    }
-
-    public void setAccountAuditId(Integer accountAuditId) {
-        this.accountAuditId = accountAuditId;
-    }
-
-    public Integer getAntiFraudAuditId() {
-        return antiFraudAuditId;
-    }
-
-    public void setAntiFraudAuditId(Integer antiFraudAuditId) {
-        this.antiFraudAuditId = antiFraudAuditId;
-    }
-
-    public Integer getPublicBankInfoAuditId() {
-        return publicBankInfoAuditId;
-    }
-
-    public void setPublicBankInfoAuditId(Integer publicBankInfoAuditId) {
-        this.publicBankInfoAuditId = publicBankInfoAuditId;
-    }
-
-    public Integer getAuthorizationAuditId() {
-        return authorizationAuditId;
-    }
-
-    public void setAuthorizationAuditId(Integer authorizationAuditId) {
-        this.authorizationAuditId = authorizationAuditId;
-    }
+    private Long authorizationAuditId;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         HistoryDTO that = (HistoryDTO) o;
-        return transferAuditId.equals(that.transferAuditId) && profileAuditId.equals(that.profileAuditId) && accountAuditId == that.accountAuditId;
+        return transferAuditId.equals(that.transferAuditId) && profileAuditId.equals(that.profileAuditId) && accountAuditId.equals(that.accountAuditId);
     }
 
     @Override
--- history/src/main/java/com/bank/history/models/History.java
+++ history/src/main/java/com/bank/history/models/History.java
@@ -1,6 +1,8 @@
 package com.bank.history.models;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Objects;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "history", schema = "history")
@@ -19,88 +23,32 @@ public class History {
     @Id
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "transfer_audit_id")
-    private Integer transferAuditId;
+    private Long transferAuditId;
 
     @Column(name = "profile_audit_id")
-    private int profileAuditId;
+    private Long profileAuditId;
 
     @Column(name = "account_audit_id")
-    private Integer accountAuditId;
+    private Long accountAuditId;
 
     @Column(name = "anti_fraud_audit_id")
-    private Integer antiFraudAuditId;
+    private Long antiFraudAuditId;
 
     @Column(name = "public_bank_info_audit_id")
-    private Integer publicBankInfoAuditId;
+    private Long publicBankInfoAuditId;
 
     @Column(name = "authorization_audit_id")
-    private Integer authorizationAuditId;
+    private Long authorizationAuditId;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         History history = (History) o;
-        return id.equals(history.id) && Objects.equals(transferAuditId, history.transferAuditId) && profileAuditId == history.profileAuditId;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getTransferAuditId() {
-        return transferAuditId;
-    }
-
-    public void setTransferAuditId(Integer transferAuditId) {
-        this.transferAuditId = transferAuditId;
-    }
-
-    public int getProfileAuditId() {
-        return profileAuditId;
-    }
-
-    public void setProfileAuditId(int profileAuditId) {
-        this.profileAuditId = profileAuditId;
-    }
-
-    public Integer getAccountAuditId() {
-        return accountAuditId;
-    }
-
-    public void setAccountAuditId(Integer accountAuditId) {
-        this.accountAuditId = accountAuditId;
-    }
-
-    public Integer getAntiFraudAuditId() {
-        return antiFraudAuditId;
-    }
-
-    public void setAntiFraudAuditId(Integer antiFraudAuditId) {
-        this.antiFraudAuditId = antiFraudAuditId;
-    }
-
-    public Integer getPublicBankInfoAuditId() {
-        return publicBankInfoAuditId;
-    }
-
-    public void setPublicBankInfoAuditId(Integer publicBankInfoAuditId) {
-        this.publicBankInfoAuditId = publicBankInfoAuditId;
-    }
-
-    public Integer getAuthorizationAuditId() {
-        return authorizationAuditId;
-    }
-
-    public void setAuthorizationAuditId(Integer authorizationAuditId) {
-        this.authorizationAuditId = authorizationAuditId;
+        return id.equals(history.id) && Objects.equals(transferAuditId, history.transferAuditId) && profileAuditId.equals(history.profileAuditId);
     }
 
     @Override
