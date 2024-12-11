d173f061b6af938b674b84a50fedd4fbdab679d1
Andrey Sidorov <andrey960683@gmail.com>
2024-12-11 22:06:49 +0300

    подправил параметры сваггера

--- public-info/src/main/java/com/bank/publicinfo/config/SwaggerConfig.java
+++ public-info/src/main/java/com/bank/publicinfo/config/SwaggerConfig.java
@@ -12,17 +12,29 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.title}")
+    private String title;
+
+    @Value("${swagger.version}")
+    private String version;
+
+    @Value("${swagger.description}")
+    private String description;
+
+    @Value("${swagger.server.description}")
+    private String serverDescription;
+
     @Value("${swagger.server.url}")
     private String serverUrl;
 
     @Bean
     public OpenAPI apiPublicInfo() {
         return new OpenAPI()
-                .info(new Info().title("Public-info API")
-                        .version("1.0")
-                        .description("Операции с банковской публичной информацией"))
+                .info(new Info().title(title)
+                        .version(version)
+                        .description(description))
                 .servers(List.of(new Server()
                         .url(serverUrl)
-                        .description("Локальный сервер для разработки")));
+                        .description(serverDescription)));
     }
 }
