a0ffd4804a76f75516a7af57e7162e1905a2e7ab
Anton200021243 <mortal0011@gmail.com>
2024-12-16 00:07:43 +0300

    Сервис Transfer Подключен к eureka серверу

--- transfer/src/main/java/com/bank/transfer/TransferApplication.java
+++ transfer/src/main/java/com/bank/transfer/TransferApplication.java
@@ -2,10 +2,12 @@ package com.bank.transfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class TransferApplication {
     public static void main(String[] args) {
 
