52aa989d9d5b148d655f5a733d2f1246473942d6
Kilbas <92kilbas@gmail.com>
2025-01-13 18:38:25 +0100

    Подключение плагина JoCoCo и написание тестов

--- antifraud/src/main/java/com/bank/antifraud/controller/SuspiciousPhoneTransfersController.java
+++ antifraud/src/main/java/com/bank/antifraud/controller/SuspiciousPhoneTransfersController.java
@@ -1,6 +1,5 @@
 package com.bank.antifraud.controller;
 
-import com.bank.antifraud.dto.SuspiciousCardTransferDTO;
 import com.bank.antifraud.dto.SuspiciousPhoneTransferDTO;
 import com.bank.antifraud.service.SuspiciousPhoneTransfersService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,7 +8,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
